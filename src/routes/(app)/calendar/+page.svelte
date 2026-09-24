21105e927306a6cf4065fb9413fde66cae41cdf5
sudy-super <rakutosu7@gmail.com>
2026-09-25 07:39:56 +0900

    refactor(ui): 予定の詳細の「記号 + 文字」の 5 行を配列から描く

--- src/routes/(app)/calendar/+page.svelte
+++ src/routes/(app)/calendar/+page.svelte
@@ -117,34 +117,31 @@
 >
 	{#if detail}
 		{@const people = detail.personIds.map((id) => personOf(db, id)?.name).filter(Boolean)}
-		<div class="list-row">
-			<Tip text="場所" name="ic-pin" size={16} />
-			<span class="tc-text">{detail.place ?? (detail.online ? 'オンライン' : '指定なし')}</span>
-		</div>
-		{#if people.length}
+		{@const rows = [
+			{ tip: '場所', icon: 'ic-pin', text: detail.place ?? (detail.online ? 'オンライン' : '指定なし') },
+			people.length && { tip: '参加者', icon: 'ic-people', text: people.join('、') },
+			detail.projectId && { tip: '案件', icon: 'ic-target', text: projectOf(db, detail.projectId)?.name },
+			detail.url && {
+				tip: detail.online === 'zoom' ? 'Zoom' : 'Meet',
+				icon: 'ic-video',
+				text: detail.url,
+				href: linkUrl(detail.url),
+				external: true
+			},
+			detail.meetingId && { tip: '会議', icon: 'ic-bell', text: '会議の準備を見る', href: `/meetings/${detail.meetingId}` }
+		].filter((r) => !!r)}
+		{#each rows as r (r.icon)}
 			<div class="list-row">
-				<Tip text="参加者" name="ic-people" size={16} />
-				<span class="tc-text">{people.join('、')}</span>
+				<Tip text={r.tip} name={r.icon} size={16} />
+				{#if r.href}
+					<a class="tc-text" href={r.href} target={r.external ? '_blank' : undefined} rel={r.external ? 'noreferrer' : undefined}
+						>{r.text}</a
+					>
+				{:else}
+					<span class="tc-text">{r.text}</span>
+				{/if}
 			</div>
-		{/if}
-		{#if detail.projectId}
-			<div class="list-row">
-				<Tip text="案件" name="ic-target" size={16} />
-				<span class="tc-text">{projectOf(db, detail.projectId)?.name}</span>
-			</div>
-		{/if}
-		{#if detail.url}
-			<div class="list-row">
-				<Tip text={detail.online === 'zoom' ? 'Zoom' : 'Meet'} name="ic-video" size={16} />
-				<a class="tc-text" href={linkUrl(detail.url)} target="_blank" rel="noreferrer">{detail.url}</a>
-			</div>
-		{/if}
-		{#if detail.meetingId}
-			<div class="list-row">
-				<Tip text="会議" name="ic-bell" size={16} />
-				<a class="tc-text" href="/meetings/{detail.meetingId}">会議の準備を見る</a>
-			</div>
-		{/if}
+		{/each}
 	{/if}
 	{#snippet actions()}
 		<button class="btn danger" onclick={remove}>削除</button>
